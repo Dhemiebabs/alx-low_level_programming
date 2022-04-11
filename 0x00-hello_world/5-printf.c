17c767aaebeffad62c2e1526f8d495d02921509e
dhemiebabs <Demilade>
2022-04-11 10:34:03 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  *
+  *Return: Always 0 (success)
+  */
+int main(void)
+{
+	char st[56] = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s\n", st);
+	return (0);
+}
