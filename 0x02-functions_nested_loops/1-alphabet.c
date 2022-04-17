2d3e17e250a49b640f6e0c057bf8ade6c6a305d0
dhemiebabs <Demilade>
2022-04-17 07:08:35 -0700

    prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Demilade Olujide Babajide
+ */
+
+#include "main.h"
+
+/**
+ * Print_alphabet - prints the alphabet, in lowercase, followed by a new line.
+ */
+
+void print_alphabet(void)
+{
+
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
